4bf8b0c9afaa472a460e8c31522feba78cbc3cf2
suncczh <suncczh@hotmail.com>
2020-11-26 17:40:18 +0800

    工程创建完成

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # irmReports
+测试
---
+++ src/test/java/com/irm/irmReports/IrmReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irm.irmReports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrmReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
